46a949e2b0fa0f8660d5e39487241886af067ea1
Nik212 <gevnik08@gmail.com>
2019-09-08 21:48:44 +0300

    WTF

---
+++ ft_memccpy.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+void *ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+    char *ptr_dst;
+    char *ptr_src;
+    int i;
+
+    i = 0;
+    ptr_dst = dest;
+    ptr_src = src;
+    while (*ptr_src != c && i != n)
+    {
+      *ptr_dst++ = *ptr_src++;
+      i++;
+    }
+    *ptr_dst++ = *ptr_src++;
+    return ptr_dst;
+}
+
+
+int main()
+{
+    char src[8] = "12345:65";
+    char dest[10];
+    ft_memccpy(dest, src, ':', 8);    
+}
