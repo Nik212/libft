887bc15b45d2756779b936d88e7c1d67631d9056
Shaunta Lupe <slupe@at-f4.21-school.ru>
2019-09-11 18:41:17 +0300

    WTF

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: slupe <slupe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 10:19:45 by slupe             #+#    #+#             */
-/*   Updated: 2019/09/11 18:39:33 by slupe            ###   ########.fr       */
+/*   Updated: 2019/09/11 18:41:15 by slupe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 void	*ft_memchr(const void *str, int c, size_t n);
+int	ft_memcmp(const void *str1, const void *str2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dest, const char *src);
