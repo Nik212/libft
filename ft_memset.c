620ce83d9a1c313c828afe50ba2f1653ac71b25e
Shaunta Lupe <slupe@at-f4.21-school.ru>
2019-09-11 18:24:36 +0300

    WTF

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: slupe <slupe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 10:43:02 by slupe             #+#    #+#             */
-/*   Updated: 2019/09/11 18:20:20 by slupe            ###   ########.fr       */
+/*   Updated: 2019/09/11 18:23:23 by slupe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memset(void *data, int c, size_t len)
 	unsigned char *p;
 	unsigned char letter;
 
-	letter = (unsigned char *)c;
-	p = (unsigned char)data;
+	letter = (unsigned char)c;
+	p = (unsigned char *)data;
 	while (len)
 	{
 		*p++ = c;
