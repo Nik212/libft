a2451e3945d316543da6fb1e559783c4afc1846a
Shaunta Lupe <slupe@at-f4.21-school.ru>
2019-09-11 12:19:34 +0300

    .h

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slupe <slupe@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/11 10:19:45 by slupe             #+#    #+#             */
+/*   Updated: 2019/09/11 12:17:34 by slupe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+void	*ft_memset(void *data, int c, int len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t len);
+void	*ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memchr(const void *str, int c, size_t n);
+
+#endif
