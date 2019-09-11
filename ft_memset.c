875279aedb2ad41779be5dc46ff9046d14d14438
Shaunta Lupe <slupe@at-f4.21-school.ru>
2019-09-11 18:27:21 +0300

    WTF

--- ft_memset.c
+++ ft_memset.c
@@ -6,11 +6,11 @@
 /*   By: slupe <slupe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 10:43:02 by slupe             #+#    #+#             */
-/*   Updated: 2019/09/11 18:25:25 by slupe            ###   ########.fr       */
+/*   Updated: 2019/09/11 18:27:11 by slupe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memset(void *data, int c, size_t len)
 {
